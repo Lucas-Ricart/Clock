783d96c4814458e93c3609540e8517715df8f39e
Lucas Ricart <lucas.ricart@laplateforme.io>
2025-01-13 10:11:01 +0100

    ajout pause, reste thread pour incrementation dans le menu

--- horloge.py
+++ horloge.py
@@ -1,89 +1,153 @@
-from datetime import datetime
 import time
+from datetime import datetime, timedelta
+import datetime
 import os
 
-heure_alarme = 99
-minute_alarme = 99
-seconde_alarme = 99
+heure = None
+alarme = None
+APM = True
+choix = False
+pause = False
 
-def horloge(heure, minute, seconde, heure_alarme, minute_alarme, seconde_alarme) :
+def afficher_heure():
+    global heure
+    heures, minutes, secondes = heure
+    horloge = datetime.time(heures, minutes, secondes)
     os.system('cls')
-    print(f'{heure:02}:{minute:02}:{seconde:02}')
-    while True :
-        if heure == heure_alarme and minute == minute_alarme and seconde == seconde_alarme-1:
-            for i in range(60) :
-                heure, minute, seconde = incrementer(heure, minute, seconde)
-                time.sleep(1)
-                os.system('cls')
-                print(f'{heure:02}:{minute:02}:{seconde:02}')
-                print("DRING DRING! C'est l'heure.")
+    if APM:
+        if heures < 12 :
+            print(horloge.strftime('AM %I:%M:%S'))
         else :
-            heure, minute, seconde = incrementer(heure, minute, seconde)
-            time.sleep(1)
-            os.system('cls')
-            print(f'{heure:02}:{minute:02}:{seconde:02}')
+            print(horloge.strftime('PM %I:%M:%S'))
+    else :
+        print(horloge.strftime('%H:%M:%S'))
 
-def choix() :
+def heure_actuelle() :
+    global heure
+    from datetime import datetime
+    while True :
+        heure = datetime.now()
+        heure = (heure.hour, heure.minute, heure.second)
+        return
+
+def régler_heure() :
+    global heure, choix
+    while True:
+        os.system('cls')
+        try:
+            if choix == True:
+                heure = input('(hh:mm:ss) : ')
+                heures, minutes, secondes = map(int, heure.split(":"))
+                if 0 <= heures < 24 and 0 <= minutes < 60 and 0 <= secondes < 60:
+                    heure = (heures, minutes, secondes)
+                    return
+                else:
+                    print('Erreur. Réessayer.')
+            elif choix == False:
+                heure_actuelle()
+                return
+            else:
+                print('Erreur. Réessayer.')
+        except ValueError:
+            print('Erreur. Réessayer.')
+        
+def régler_alarme() :
+    global alarme
+    while True:
+        os.system('cls')
+        try:
+            alarme = input('(hh:mm:ss) : ')
+            heures, minutes, secondes = map(int, alarme.split(":"))
+            if 0 <= heures < 24 and 0 <= minutes < 60 and 0 <= secondes < 60:
+                alarme = (heures, minutes, secondes)
+                return
+            else :
+                print('Erreur. Réessayer.')
+        except ValueError:
+            print('Erreur. Réessayer.')
+
+def test_alarme() :
+    global alarme, heure
+    if heure == alarme :
+        for i in range(30) :
+            afficher_heure()
+            print("Dring Dring ! Il est l'heure !")
+            time.sleep(1)
+
+def incrémenter() :
+    global heure, pause
+    heures, minutes, secondes = heure
+    while True :
+        if pause == True :
+            return
+        else :
+            secondes += 1
+            if secondes == 60:
+                secondes = 0
+                minutes += 1
+            if minutes == 60:
+                minutes = 0
+                heures += 1
+            if heures == 24:
+                heures = 0
+            heure = (heures, minutes, secondes)
+            return
+    
+def selection() :
+    global choix, APM, pause
     test = 0
     while True :
         os.system('cls')
-        if test > 3 :
-            print('Erreur. Rééssayer.')
         print("1. Lancer l'horloge")
-        print("2. Régler l'heure")
-        print("3. Régler une alarme")
+        if choix == False :
+             print("2. Régler l'heure")
+        else :
+            print("2. Utiliser l'heure locale")
+        print('3. Régler une alarme')
+        if APM == False :
+            print('4. Passer en format 12h')
+        else :
+            print('4. Passer en format 24h')
+        if pause == False :
+            print('5. Mettre en pause')
+        else :
+            print('5. Couper la pause')
         test=int(input())
-        if test == 1 or test == 2 or test == 3 :
-            return test
-        
-def incrementer(heure, minute, seconde) :
-    seconde += 1
-    if seconde == 60 :
-         seconde = 0
-         minute += 1
-    if minute == 60 :
-        minute = 0
-        heure += 1
-    if heure == 24 :
-        heure = 0
-    return heure, minute, seconde
-                   
-def heure_machine() :
-    while True :
-        os.system('cls')
-        maintenant=datetime.now()
-        return maintenant.hour, maintenant.minute, maintenant.second
-
-def afficher_heure() :
-    while True :
-        horloge = input("(hh:mm:ss) : ")
         try :
-            heure, minute, seconde = map(int,horloge.split(':'))
-            if 0 <= heure < 24 and 0 <= minute < 60 and 0 <= seconde < 60 :
-                return heure, minute, seconde
-            else :
-                print('Erreur. Rééssayer.')
+            if test == 2 :
+                if choix == False :
+                    choix = True
+                    régler_heure()
+                else :
+                    choix = False
+                    régler_heure()
+            elif test == 3 :
+                régler_alarme()
+            elif test == 4 :
+                if APM == False :
+                    APM = True
+                else :
+                    APM = False
+            elif test == 5 :
+                if pause == False :
+                    pause = True
+                else :
+                    pause = False
+            return
         except ValueError :
-            os.system('cls')
-            print('Erreur. Rééssayer.')
-
-def définir_alarme() :
+            print('Erreur. Réessayer.')
+def main() :
+    heure_actuelle()
     while True :
-        horloge = input("(hh:mm:ss) : ")
+        selection()
         try :
-            heure, minute, seconde = map(int,horloge.split(':'))
-            if 0 <= heure < 24 and 0 <= minute < 60 and 0 <= seconde < 60 :
-                return heure, minute, seconde
-            else :
-                print('Erreur. Rééssayer.')
-        except ValueError :
-            os.system('cls')
-            print('Erreur. Rééssayer.')
+            while True :
+                afficher_heure()
+                incrémenter()
+                test_alarme()
+                time.sleep(1)
+        except KeyboardInterrupt :
+            None
 
-test=choix()
-heure, minute, seconde = heure_machine()
-if test == 2 :
-    heure, minute, seconde = afficher_heure()
-elif test == 3 :
-    heure_alarme, minute_alarme, seconde_alarme = définir_alarme()
-horloge(heure, minute, seconde, heure_alarme, minute_alarme, seconde_alarme)
+
+main()
